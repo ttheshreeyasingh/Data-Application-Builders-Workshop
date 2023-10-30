fc3091e9529251b7fc932e697ef1df4dabc0c240
Shreeya <82967080+ttheshreeyasingh@users.noreply.github.com>
2023-10-30 15:11:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
+import pandas
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # showing API response in a dataframe
