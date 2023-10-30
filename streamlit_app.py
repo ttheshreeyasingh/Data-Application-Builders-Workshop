f5e15713191794c32b073a5baaa7b8ecf097fd0d
Shreeya <82967080+ttheshreeyasingh@users.noreply.github.com>
2023-10-30 15:15:24 +0530

    Added text entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
+import requests
 import pandas
+
 streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Favourites')
@@ -19,12 +21,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-import requests
-import pandas
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # showing API response in a dataframe
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 
