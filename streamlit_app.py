9c320636cc54e0fc2120520d85b8f0956041ad5a
Shreeya <82967080+ttheshreeyasingh@users.noreply.github.com>
2023-10-30 15:03:30 +0530

    fixed bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 # showing API response in a dataframe
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
